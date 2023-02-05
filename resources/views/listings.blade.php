1a2ea077cc680e0e25bda463988803fe11447cf5
Yohanes Silitonga <yohanesrfsilitonga21@gmail.com>
2023-02-05 22:30:52 +0700

    adding

--- resources/views/listings.blade.php
+++ resources/views/listings.blade.php
@@ -11,3 +11,17 @@
         <p> {{ $listing['description'] }}</p>
     @endforeach
 </h1>
+
+<h1>Listing
+
+    <h1>{{ $headings }}</h1>
+
+    @if (count($listings) === 0)
+        <p>No listings Found</p>
+    @endif
+
+    @foreach ($listings as $listing)
+        <h2> <a href="/listing/{{ $listing['id'] }}">{{ $listing['title'] }}<a></h2>
+        <p> {{ $listing['description'] }}</p>
+    @endforeach
+</h1>
